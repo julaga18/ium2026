d1c7f77c7fe625700afe413f745dcef83fc7522e
s465090 <julgad2@st.amu.edu.pl>
2026-03-25 08:52:02 +0100

    Modify script

--- scripts/download_and_process.sh
+++ scripts/download_and_process.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-CUTOFF=$1
-mkdir -p data
+set -e
 
-seq 1 100 > data/raw_dataset.txt
-echo "Pobrano zbiór danych do data/raw_dataset.txt"
+LIMIT=${1:-15}
 
-shuf data/raw_dataset.txt | head -n $CUTOFF > data/subset.txt
-echo "Utworzono podzbiór $CUTOFF losowych elementów w data/subset.txt"
-
-head -n $CUTOFF data/subset.txt > data/final_dataset.txt
-echo "Utworzono finalny zbiór danych z $CUTOFF przykładami w data/final_dataset.txt"
+head -n 1 data/diabetes.csv > data/dataset.csv
+tail -n +2 data/diabetes.csv | shuf | head -n "$LIMIT" >> data/dataset.csv
